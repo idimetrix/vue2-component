b6335ecc7af17c864de8dc896290639cc5b06a3f
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2020-01-08 17:15:52 +0300

    v1

--- src/component/index.js
+++ src/component/index.js
@@ -1,9 +1,21 @@
+import Component from "./Component";
+
 const plugin = {
-  install(Vue, options = {}) {}
+  install(Vue, options = {}) {
+    const { option1, option2, option3 } = {
+      option1: null,
+      option2: null,
+      option3: null,
+      ...options
+    };
+
+    console.log(option1, option2, option3);
+  }
 };
 
 export default plugin;
 
 if (typeof window !== `undefined` && window.Vue) {
   window.Vue.use(plugin);
+  window.Component = Component;
 }
